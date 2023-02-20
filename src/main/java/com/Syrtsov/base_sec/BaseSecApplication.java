90d40ba109d6b69696e8420f99070d9254e581ab
YodaB <ivansyrcov26@gmail.com>
2023-02-20 21:37:41 +0300

    Первый коммит

---
+++ src/main/java/com/Syrtsov/base_sec/BaseSecApplication.java
@@ -0,0 +1,13 @@
+package com.Syrtsov.base_sec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseSecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Syrtsov/base_sec/BaseSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Syrtsov.base_sec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
